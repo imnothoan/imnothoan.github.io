89b6b9899b2a0778f6de23521cc8bfd595d707a9
imnothoan local <imnothoan@local.com>
2025-12-18 17:22:08 +0700

    Deploy AIforLife Frontend #13 with https

--- assets/eye-DHMALM6Y.js
+++ assets/eye-DHMALM6Y.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-N2kES7nl.js";/**
+import{c as e}from"./index-CKhVfOUJ.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/proxy-dxJY_AFk.js
+++ assets/proxy-dxJY_AFk.js
@@ -1,4 +1,4 @@
-import{c as Ls,r as x,j as _}from"./index-N2kES7nl.js";/**
+import{c as Ls,r as x,j as _}from"./index-CKhVfOUJ.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
